e4ed19c0014435f6a50f62d7a6a4671368db282b
Noah Zinn <noahwzinn@gmail.com>
2025-01-23 22:24:49 -0600

    Modified javascript test

--- Tests/postPerson.test.js
+++ Tests/postPerson.test.js
@@ -1,21 +1,21 @@
 const postPerson = require('./postPerson')
 
 test('Full request', async () => {
-  expect(await postPerson({name: "Bob", id: 123, age: 20, location: "Alaska"})).toBe(201);
+  expect(await postPerson({id: 123, name: "Bob Miller", age: 40, location: "Alaska"})).toBe(201);
 })
 
 test('Partial request', async () => {
-  expect(await postPerson({name: "Bob", id: 124})).toBe(201);
+  expect(await postPerson({ id: 124, name: 'Bill Stephenson', age: 34 })).toBe(201);
 })
 
 test('Missing name', async () => {
-  expect(await postPerson({id: 123})).toBe(400);
+  expect(await postPerson({ id: 125, age: 23 })).toBe(400);
 })
 
 test('Missing id', async () => {
-  expect(await postPerson({name: "Bob"})).toBe(400);
+  expect(await postPerson({name: "Stephen Michael", age: 10})).toBe(400);
 })
 
 test('Negative age', async () => {
-  expect(await postPerson({id: 123, name: "Bob", age: -1}))
+  expect(await postPerson({id: 126, name: "Sarah Steinbeck", age: -2}))
 })
