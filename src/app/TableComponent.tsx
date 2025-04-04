d90a00b18b02b123e8b5a6b628dd1a115b70f490
cth2x <colehicks50@gmail.com>
2025-04-04 00:44:43 -0500

    Fixed some bugs on the table page

--- src/app/TableComponent.tsx
+++ src/app/TableComponent.tsx
@@ -1,41 +1,16 @@
 //Table layout
 'use client';
 
-import {
-  createTheme,
-  ThemeProvider,
-  useMediaQuery,
-  useTheme,
-  IconButton,
-  Tooltip,
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-  Box,
-} from '@mui/material';
+import React, { useState, useEffect, FormEvent } from 'react';
 import {
   MaterialReactTable,
-  MRT_ColumnDef,
-  MRT_ShowHideColumnsButton,
-  MRT_ToggleFullScreenButton,
-  MRT_ToolbarInternalButtons,
   useMaterialReactTable,
+  type MRT_ColumnDef,
 } from 'material-react-table';
-
-import { LocalizationProvider } from '@mui/x-date-pickers'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-
-import { useMemo, useState, useEffect } from 'react';
-import { MissingPerson } from './table/page';
-import LogoutIcon from '@mui/icons-material/Logout';
-import LoginIcon from '@mui/icons-material/Login';
-import DownloadIcon from '@mui/icons-material/Download';
 import { useRouter } from 'next/navigation';
-import FormDrawer from './utils/FormDrawer';
-import { FormEvent } from 'react';
+import { MissingPerson } from './table/page';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 type TableComponentProps = {
   columns: MRT_ColumnDef<MissingPerson>[];
@@ -44,88 +19,6 @@ type TableComponentProps = {
 
 const TableComponent = ({ columns, data }: TableComponentProps) => {
   const router = useRouter();
-  const [openLogoutDialog, setOpenLogoutDialog] = useState(false);
-  const [isLoggingOut, setIsLoggingOut] = useState(false);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [showLoginModal, setShowLoginModal] = useState(false);
-  const [session, setSession] = useState(null);
-  
-  useEffect(() => {
-    // Check authentication status when component mounts
-    const checkAuthStatus = async () => {
-      try {
-        const response = await fetch('/api/auth');
-        if (response.ok) {
-          const data = await response.json();
-          setIsAuthenticated(data.isAuthenticated);
-        }
-      } catch (error) {
-        console.error('Error checking authentication status:', error);
-      }
-    };
-
-    // Get session for admin check
-    const getSession = async () => {
-      const res = await fetch('/api/session');
-      setSession(await res.json());
-    }
-
-    checkAuthStatus();
-    getSession();
-  }, []);
-
-  const handleLogoutClick = () => {
-    setOpenLogoutDialog(true);
-  };
-
-  const handleLoginClick = () => {
-    setShowLoginModal(true);
-  };
-
-  const handleCloseDialog = () => {
-    setOpenLogoutDialog(false);
-  };
-
-  const handleCloseLoginModal = () => {
-    setShowLoginModal(false);
-  };
-
-  async function handleLogin(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-
-    const formData = new FormData(event.currentTarget);
-    const response = await fetch('/api/login', {
-      method: 'POST',
-      body: formData,
-    });
-
-    if (response.ok) {
-      setShowLoginModal(false);
-      setIsAuthenticated(true);
-      window.location.reload();
-    }
-  }
-
-  const handleLogout = async () => {
-    try {
-      setIsLoggingOut(true);
-      const response = await fetch('/api/logout', {
-        method: 'POST',
-      });
-
-      if (response.ok) {
-        setIsLoggingOut(false);
-        setOpenLogoutDialog(false);
-        setIsAuthenticated(false);
-	// Reload window to get appropriate effect on table
-	window.location.reload();
-      }
-    } catch (error) {
-      console.error('Logout failed:', error);
-      setIsLoggingOut(false);
-      setOpenLogoutDialog(false);
-    }
-  };
 
   const handleRowClick = (row: any) => {
     router.push(`/profile/${row.original.case_id}`);
@@ -136,7 +29,6 @@ const TableComponent = ({ columns, data }: TableComponentProps) => {
     id: 'missing-persons-table',
     enableColumnOrdering: true,
     enableColumnPinning: true,
-    // enableRowActions: true,
     enableColumnActions: false,
     enableColumnDragging: false,
     data,
@@ -146,24 +38,6 @@ const TableComponent = ({ columns, data }: TableComponentProps) => {
       size: 40,
     },
     initialState: { columnPinning: { left: ['actions'] } },
-    renderTopToolbarCustomActions: ({ table }) => (
-      <Box sx={{ display: 'flex', gap: 2, alignItems: 'center' }}>
-        {/* Only load missing person button when logged in as admin */}
-        {(session && session.role === 'admin') &&
-	  <Button variant="contained" onClick={() => openDrawer()}>
-          Add Missing Person
-         </Button>}
-
-        {/* Export to CSV button */}
-        <Button
-          variant="outlined"
-          color="primary"
-          onClick={exportToCSV}
-          startIcon={<DownloadIcon />}>
-          Export CSV
-        </Button>
-    </Box>
-    ),
     muiTableBodyProps: {
       sx: {
         '& tr:nth-of-type(odd) > td': {
@@ -182,166 +56,13 @@ const TableComponent = ({ columns, data }: TableComponentProps) => {
     }),
   });
 
-  // Function to export table data to CSV
-  const exportToCSV = () => {
-    // Get visible columns
-    const visibleColumns = table.getVisibleLeafColumns().map((column) => {
-      return {
-        id: column.id,
-        header: column.columnDef.header?.toString() || column.id,
-      };
-    });
-
-    // Create CSV header row
-    const headerRow = visibleColumns
-      .map((column) => `"${column.header}"`)
-      .join(',');
-
-    // Create CSV data rows
-    const dataRows = data.map((row) => {
-      return visibleColumns
-        .map((column) => {
-          const cellValue = row[column.id as keyof MissingPerson];
-          // Handle different data types and escape quotes
-          return `"${cellValue?.toString().replace(/"/g, '""') || ''}"`;
-        })
-        .join(',');
-    });
-
-    // Combine header and data rows
-    const csvContent = [headerRow, ...dataRows].join('\n');
-
-    // Create a Blob and download link
-    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
-    const url = URL.createObjectURL(blob);
-    const link = document.createElement('a');
-    link.setAttribute('href', url);
-    link.setAttribute('download', 'missing_persons_data.csv');
-    link.style.visibility = 'hidden';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
-
-  const [drawerOpen, setDrawerOpen] = useState(false);
-  const openDrawer = (person: MissingPerson | null = null) => {
-    setDrawerOpen(true);
-  };
-
   return (
     <>
-      <div className="fixed top-4 right-4 z-50">
-        {isAuthenticated ? (
-          <Button
-            variant="contained"
-            color="primary"
-            startIcon={<LogoutIcon />}
-            onClick={handleLogoutClick}
-            size="medium"
-            sx={{ minWidth: '100px' }}>
-            Logout
-          </Button>
-        ) : (
-          <Button
-            variant="contained"
-            color="primary"
-            startIcon={<LoginIcon />}
-            onClick={handleLoginClick}
-            size="medium"
-            sx={{ minWidth: '100px' }}>
-            Admin Login
-          </Button>
-        )}
-      </div>
-
-      {/* Login Modal */}
-      {showLoginModal && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-          <div className="bg-white p-8 rounded-lg shadow-lg w-96">
-            <div className="flex justify-between items-center mb-6">
-              <h3 className="text-xl font-semibold">Admin Login</h3>
-              <button
-                onClick={handleCloseLoginModal}
-                className="text-gray-500 hover:text-gray-700">
-                ✕
-              </button>
-            </div>
-            <form className="space-y-4" onSubmit={handleLogin}>
-              <div>
-                <label className="block text-sm font-medium text-gray-700">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
-                />
-              </div>
-              <div>
-                <label className="block text-sm font-medium text-gray-700">
-                  Password
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
-                />
-              </div>
-              <button
-                type="submit"
-                className="w-full px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition font-semibold">
-                Sign In
-              </button>
-            </form>
-          </div>
-        </div>
-      )}
-
-      <Dialog
-        open={openLogoutDialog}
-        onClose={handleCloseDialog}
-        aria-labelledby="logout-dialog-title"
-        aria-describedby="logout-dialog-description">
-        <DialogTitle
-          id="logout-dialog-title"
-          sx={{ textAlign: 'center', paddingBottom: 1 }}>
-          Confirm Logout
-        </DialogTitle>
-        <DialogContent sx={{ paddingTop: 0, paddingBottom: 1 }}>
-          <DialogContentText
-            id="logout-dialog-description"
-            sx={{ textAlign: 'center' }}>
-            Are you sure you want to log out? You will still be able to view the
-            table, but you won't be able to make any changes until you log in
-            again.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions
-          sx={{ justifyContent: 'center', paddingTop: 0, paddingBottom: 2 }}>
-          <Button onClick={handleCloseDialog} color="primary">
-            Cancel
-          </Button>
-          <Button
-            onClick={handleLogout}
-            color="error"
-            variant="contained"
-            disabled={isLoggingOut}>
-            {isLoggingOut ? 'Logging out...' : 'Logout'}
-          </Button>
-        </DialogActions>
-      </Dialog>
-
       <div className="shadow-md rounded-lg">
-  	<LocalizationProvider dateAdapter={AdapterDayjs}>
-          <MaterialReactTable table={table}/>
-	</LocalizationProvider>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <MaterialReactTable table={table} />
+        </LocalizationProvider>
       </div>
-
-      <FormDrawer
-        open={drawerOpen}
-        onClose={() => setDrawerOpen(false)}
-        initialData={undefined}
-      />
     </>
   );
 };
--- src/app/table/page.tsx
+++ src/app/table/page.tsx
@@ -1,12 +1,25 @@
 //Displays table with a persons data
 'use client';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, FormEvent } from 'react';
 import Link from 'next/link';
 import TableComponent from '../TableComponent';
 import { fetchMissingPersons } from '../utils/fetch';
 import { MRT_ColumnDef } from 'material-react-table';
-import { Button, IconButton } from '@mui/material';
+import {
+  Button,
+  IconButton,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Box,
+} from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import LoginIcon from '@mui/icons-material/Login';
+import LogoutIcon from '@mui/icons-material/Logout';
+import AddIcon from '@mui/icons-material/Add';
+import DownloadIcon from '@mui/icons-material/Download';
 import { EditIcon } from 'lucide-react';
 import FormDrawer from '@/utils/FormDrawer';
 
@@ -28,15 +41,34 @@ export type MissingPerson = {
   category_of_missing: string;
 };
 
+// Define a type for the session state
+type SessionData = {
+  user?: {
+    name?: string | null;
+    email?: string | null;
+    image?: string | null;
+  };
+  role?: string;
+  expires?: string;
+} | null;
+
 export default function TablePage() {
-  const [data, setData] = useState([]);
-  const [session, setSession] = useState(null);
+  const [data, setData] = useState<MissingPerson[]>([]);
+  const [session, setSession] = useState<SessionData>(null);
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [selectedPerson, setSelectedPerson] = useState<MissingPerson | null>(
+    null
+  );
+
+  // Authentication and Modal/Dialog State from TableComponent
+  const [openLogoutDialog, setOpenLogoutDialog] = useState(false);
+  const [isLoggingOut, setIsLoggingOut] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [showLoginModal, setShowLoginModal] = useState(false);
 
   useEffect(() => {
     async function loadData() {
       const result = await fetchMissingPersons();
-
-      // Transform data: Split "name" into "first_name" and "last_name"
       const transformedData = result.map((person: any) => {
         const nameParts = person.name.split(' ');
         const last_name = nameParts[0];
@@ -56,140 +88,198 @@ export default function TablePage() {
           category_of_missing: person.classification || 'N/A',
         };
       });
-
-
       setData(transformedData);
     }
 
-    async function getSession() {
-      const res = await fetch('/api/session');
-      const data = await res.json();
-      setSession(data);
-      console.log("Session:", data);
+    async function getSessionAndAuth() {
+      try {
+        // Fetch session
+        const sessionRes = await fetch('/api/session');
+        const sessionData = await sessionRes.json();
+        setSession(sessionData);
+        console.log('Session:', sessionData);
+
+        // Fetch auth status
+        const authResponse = await fetch('/api/auth');
+        if (authResponse.ok) {
+          const authData = await authResponse.json();
+          setIsAuthenticated(authData.isAuthenticated);
+        } else {
+          setIsAuthenticated(false);
+        }
+      } catch (error) {
+        console.error('Error fetching session or auth status:', error);
+        setIsAuthenticated(false);
+        setSession(null);
+      }
     }
 
     loadData();
-    getSession();
+    getSessionAndAuth();
   }, []);
-  const [drawerOpen, setDrawerOpen] = useState(false);
-  const [selectedPerson, setSelectedPerson] = useState<MissingPerson | null>(
-    null
-  );
 
-  // Open drawer here is causing some errors:
+  // Handlers from TableComponent
+  const handleLogoutClick = () => {
+    setOpenLogoutDialog(true);
+  };
+
+  const handleLoginClick = () => {
+    setShowLoginModal(true);
+  };
+
+  const handleCloseDialog = () => {
+    setOpenLogoutDialog(false);
+  };
+
+  const handleCloseLoginModal = () => {
+    setShowLoginModal(false);
+  };
+
+  async function handleLogin(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const response = await fetch('/api/login', {
+      method: 'POST',
+      body: formData,
+    });
+
+    if (response.ok) {
+      setShowLoginModal(false);
+      window.location.reload();
+    } else {
+      console.error('Login failed');
+    }
+  }
+
+  const handleLogout = async () => {
+    try {
+      setIsLoggingOut(true);
+      const response = await fetch('/api/logout', { method: 'POST' });
+      if (response.ok) {
+        window.location.reload();
+      } else {
+        console.error('Logout failed');
+        setIsLoggingOut(false);
+        setOpenLogoutDialog(false);
+      }
+    } catch (error) {
+      console.error('Logout failed:', error);
+      setIsLoggingOut(false);
+      setOpenLogoutDialog(false);
+    }
+  };
+
   const openDrawer = (person: MissingPerson | null = null) => {
-    console.log('Opening Drawer with Person:', person); // Debugging log
+    console.log('Opening Drawer with Person:', person);
     setSelectedPerson(person);
     setDrawerOpen(true);
   };
 
+  let columns: MRT_ColumnDef<MissingPerson>[] = [];
 
-
-  let columns: MRT_ColumnDef<MissingPerson>[];
-  
   if (session && session.role === 'admin') {
     columns = [
-    // Make this only show up for admin
-    {
-      header: 'Edit',
-      id: 'actions',
-      enableSorting: false,
-      enableColumnFilter: false,
-      Cell: ({ row }) => (
-        <IconButton
-          className="edit-button"
-          onClick={(event) => {
-            event.stopPropagation(); // Prevent row click behavior (if re-added later)
-            console.log('Edit button clicked for:', row.original); // Debugging log
-            openDrawer(row.original);
-          }}>
-          <EditIcon />
-        </IconButton>
-      ),
-    },
-    { accessorKey: 'case_id', header: 'ID' },
-    { accessorKey: 'first_name', header: 'First Name' },
-    { accessorKey: 'last_name', header: 'Last Name' },
-    { accessorKey: 'age', header: 'Age', filterVariant: 'range-slider'},
-    { accessorKey: 'gender', header: 'Sex' },
-    { accessorKey: 'race', header: 'Race / Ethnicity' },
-    {
-      accessorFn: (originalRow) => new Date(originalRow.missing_date),
-      header: 'Date Missing',
-      Cell: ({ cell }) => cell.getValue<Date>().toLocaleDateString(),
-      filterVariant: 'date-range'
-    },
-    { accessorKey: 'city', header: 'City' },
-    { accessorKey: 'county', header: 'County' },
+      {
+        header: 'Edit',
+        id: 'actions',
+        enableSorting: false,
+        enableColumnFilter: false,
+        Cell: ({ row }) => (
+          <IconButton
+            className="edit-button"
+            onClick={(event) => {
+              event.stopPropagation();
+              console.log('Edit button clicked for:', row.original);
+              openDrawer(row.original);
+            }}>
+            <EditIcon />
+          </IconButton>
+        ),
+        size: 60,
+      },
+      { accessorKey: 'case_id', header: 'ID' },
+      { accessorKey: 'first_name', header: 'First Name' },
+      { accessorKey: 'last_name', header: 'Last Name' },
+      { accessorKey: 'age', header: 'Age', filterVariant: 'range-slider' },
+      { accessorKey: 'gender', header: 'Sex' },
+      { accessorKey: 'race', header: 'Race / Ethnicity' },
+      {
+        accessorFn: (originalRow) => new Date(originalRow.missing_date),
+        header: 'Date Missing',
+        Cell: ({ cell }) => cell.getValue<Date>().toLocaleDateString(),
+        filterVariant: 'date-range',
+      },
+      { accessorKey: 'city', header: 'City' },
+      { accessorKey: 'county', header: 'County' },
 
-    {
-      accessorKey: 'tribe_statuses',
-      header: 'Tribal Statuses',
-      Cell: ({ row }) => row.original.tribe_statuses?.join(', ') || 'N/A',
-    },
-    {
-      accessorKey: 'tribes',
-      header: 'Associated Tribes',
-      Cell: ({ row }) => row.original.tribes?.join(', ') || 'N/A',
-      filterVariant: 'select',
-    },
-    {
-      accessorKey: 'classification',
-      header: 'Category of Missing',
-      Cell: ({ row }) => row.original.classification || 'N/A',
-    },
-    {
-      accessorFn: (originalRow) => new Date(originalRow.date_modified),
-      header: 'Date modified',
-      filterVariant: 'date-range' ,
-      Cell: ({ cell }) => cell.getValue<Date>().toLocaleDateString()
-    }
+      {
+        accessorKey: 'tribe_statuses',
+        header: 'Tribal Statuses',
+        Cell: ({ row }) => row.original.tribe_statuses?.join(', ') || 'N/A',
+      },
+      {
+        accessorKey: 'tribes',
+        header: 'Associated Tribes',
+        Cell: ({ row }) => row.original.tribes?.join(', ') || 'N/A',
+        filterVariant: 'select',
+      },
+      {
+        accessorKey: 'classification',
+        header: 'Category of Missing',
+        Cell: ({ row }) => row.original.classification || 'N/A',
+      },
+      {
+        accessorFn: (originalRow) => new Date(originalRow.date_modified),
+        header: 'Date modified',
+        filterVariant: 'date-range',
+        Cell: ({ cell }) => cell.getValue<Date>().toLocaleDateString(),
+      },
     ];
   } else {
     columns = [
       { accessorKey: 'case_id', header: 'ID' },
       { accessorKey: 'first_name', header: 'First Name' },
       { accessorKey: 'last_name', header: 'Last Name' },
-      { accessorKey: 'age', header: 'Age', filterVariant: 'range-slider'},
+      { accessorKey: 'age', header: 'Age', filterVariant: 'range-slider' },
       { accessorKey: 'gender', header: 'Sex' },
       { accessorKey: 'race', header: 'Race / Ethnicity' },
       {
-	accessorFn: (originalRow) => new Date(originalRow.missing_date),
-	header: 'Date Missing',
-	Cell: ({ cell }) => cell.getValue<Date>().toLocaleDateString(),
-	filterVariant: 'date-range'
+        accessorFn: (originalRow) => new Date(originalRow.missing_date),
+        header: 'Date Missing',
+        Cell: ({ cell }) => cell.getValue<Date>().toLocaleDateString(),
+        filterVariant: 'date-range',
       },
       { accessorKey: 'city', header: 'City' },
       { accessorKey: 'county', header: 'County' },
-      
+
       {
-	accessorKey: 'tribe_statuses',
-	header: 'Tribal Statuses',
-	Cell: ({ row }) => row.original.tribe_statuses?.join(', ') || 'N/A',
+        accessorKey: 'tribe_statuses',
+        header: 'Tribal Statuses',
+        Cell: ({ row }) => row.original.tribe_statuses?.join(', ') || 'N/A',
       },
       {
-	accessorKey: 'tribes',
-	header: 'Associated Tribes',
-	Cell: ({ row }) => row.original.tribes?.join(', ') || 'N/A',
-	filterVariant: 'select',
+        accessorKey: 'tribes',
+        header: 'Associated Tribes',
+        Cell: ({ row }) => row.original.tribes?.join(', ') || 'N/A',
+        filterVariant: 'select',
       },
       {
-	accessorKey: 'classification',
-	header: 'Category of Missing',
-	Cell: ({ row }) => row.original.classification || 'N/A',
+        accessorKey: 'classification',
+        header: 'Category of Missing',
+        Cell: ({ row }) => row.original.classification || 'N/A',
       },
       {
-	accessorFn: (originalRow) => new Date(originalRow.date_modified),
-	header: 'Date modified',
-	filterVariant: 'date-range' ,
-	Cell: ({ cell }) => cell.getValue<Date>().toLocaleDateString()
-      }
+        accessorFn: (originalRow) => new Date(originalRow.date_modified),
+        header: 'Date modified',
+        filterVariant: 'date-range',
+        Cell: ({ cell }) => cell.getValue<Date>().toLocaleDateString(),
+      },
     ];
   }
-  
+
   return (
-    <div className="p-6 h-full ">
-      <div className="fixed top-4 left-4 z-50">
+    <div className="p-6 h-full">
+      <div className="flex justify-between items-center mb-4 gap-4">
         <Link href="/">
           <Button
             variant="contained"
@@ -204,20 +294,132 @@ export default function TablePage() {
             Back
           </Button>
         </Link>
+
+        <h1 className="text-2xl font-bold text-center flex-grow">
+          Missing Persons Table
+        </h1>
+
+        <Box sx={{ display: 'flex', gap: 2, alignItems: 'center' }}>
+          {session && session.role === 'admin' && (
+            <Button
+              variant="contained"
+              onClick={() => openDrawer()}
+              startIcon={<AddIcon />}
+              size="medium">
+              Add Person
+            </Button>
+          )}
+          {isAuthenticated ? (
+            <Button
+              variant="contained"
+              color="primary"
+              startIcon={<LogoutIcon />}
+              onClick={handleLogoutClick}
+              size="medium"
+              sx={{ minWidth: '100px' }}>
+              Logout
+            </Button>
+          ) : (
+            <Button
+              variant="contained"
+              color="primary"
+              startIcon={<LoginIcon />}
+              onClick={handleLoginClick}
+              size="medium"
+              sx={{ minWidth: '100px' }}>
+              Admin Login
+            </Button>
+          )}
+        </Box>
       </div>
 
-      <div className="fixed top-4 left-1/2 transform -translate-x-1/2 z-50">
-        <h1 className="text-2xl font-bold">Missing Persons Table</h1>
-      </div>
-
-      <div className="pt-16">
+      <div>
         <TableComponent columns={columns} data={data} />
       </div>
 
+      {showLoginModal && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+          <div className="bg-white p-8 rounded-lg shadow-lg w-96">
+            <div className="flex justify-between items-center mb-6">
+              <h3 className="text-xl font-semibold">Admin Login</h3>
+              <button
+                onClick={handleCloseLoginModal}
+                className="text-gray-500 hover:text-gray-700">
+                ✕
+              </button>
+            </div>
+            <form className="space-y-4" onSubmit={handleLogin}>
+              <div>
+                <label className="block text-sm font-medium text-gray-700">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  required
+                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                />
+              </div>
+              <div>
+                <label className="block text-sm font-medium text-gray-700">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  required
+                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-full px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition font-semibold">
+                Sign In
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
+
+      <Dialog
+        open={openLogoutDialog}
+        onClose={handleCloseDialog}
+        aria-labelledby="logout-dialog-title"
+        aria-describedby="logout-dialog-description">
+        <DialogTitle
+          id="logout-dialog-title"
+          sx={{ textAlign: 'center', pb: 1 }}>
+          Confirm Logout
+        </DialogTitle>
+        <DialogContent sx={{ pt: 0, pb: 1 }}>
+          <DialogContentText
+            id="logout-dialog-description"
+            sx={{ textAlign: 'center' }}>
+            Are you sure you want to log out?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions sx={{ justifyContent: 'center', pt: 0, pb: 2 }}>
+          <Button onClick={handleCloseDialog} color="primary">
+            Cancel
+          </Button>
+          <Button
+            onClick={handleLogout}
+            color="error"
+            variant="contained"
+            disabled={isLoggingOut}>
+            {isLoggingOut ? 'Logging out...' : 'Logout'}
+          </Button>
+        </DialogActions>
+      </Dialog>
+
       <FormDrawer
         open={drawerOpen}
-        onClose={() => setDrawerOpen(false)}
-        initialData={selectedPerson || undefined}></FormDrawer>
+        onClose={() => {
+          setDrawerOpen(false);
+          setSelectedPerson(null);
+        }}
+        initialData={selectedPerson || undefined}
+      />
     </div>
   );
 }
